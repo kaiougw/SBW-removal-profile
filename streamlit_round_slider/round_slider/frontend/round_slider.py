34d861a116fd278860daaffd5a0f7c0e30da671c
kaijwou <kaijwou@gmail.com>
2025-09-16 08:31:39 +0800

    Update round_slider.py

--- streamlit_round_slider/round_slider/frontend/round_slider.py
+++ streamlit_round_slider/round_slider/frontend/round_slider.py
@@ -1,1 +1,11 @@
+import os
+import streamlit.components.v1 as components
+
+_component_func = components.declare_component(
+    "round_slider",
+    path=os.path.join(os.path.dirname(__file__), "frontend/build"),
+)
+
+def round_slider(key=None) -> int:
+    return _component_func(key=key, default=0)
 
