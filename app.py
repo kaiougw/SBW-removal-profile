cf00796cf4faf05c3807778f6cb00c993011ae06
kaijwou <kaijwou@gmail.com>
2025-09-16 09:59:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 import streamlit as st
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from streamlit_round_slide import round_slider
+from streamlit_round_slider import round_slider
 
 def safe_get(d: dict, key: str, default=None):
     return d.get(key, default) if isinstance(d, dict) else default
