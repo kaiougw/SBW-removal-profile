d351a7b6c8c5c337a30a4af48514c5791f83fcda
kaiougw <kaiou@gw-semi.com>
2025-10-03 14:45:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def parsesbw(sbwfile: str) -> sbwinfo:
                         for j in range(1, len(rptcol)):  # skip "No" at index 0
                             _row[rptcol[j]] = tmpstr[j]
                         sbw.SummaryReport.append(_row)\
-                        break
+                        # break
             elif tmpline[0] == '[MeasureData.PointsDataList]':
                 sbw.WaferData.clear()
                 waferno=tmpline[1]
