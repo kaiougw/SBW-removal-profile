e46aff520eb74e1ddb1ebc29407b301469ac4ea7
kaiougw <kaiou@gw-semi.com>
2025-10-27 10:40:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1021,7 +1021,7 @@ with colDiv:
         """
         <div style="
             width: 100%;
-            height: 100%;
+            height: 150px;
             display: flex;
             align-items: center;
             justify-content: center;
